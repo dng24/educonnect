e48b41b72a6226d865b349a0cef4ad9dbc8beb17
Derek Ng <37806620+dng24@users.noreply.github.com>
2024-03-30 22:12:23 -0400

    rate my prof

---
+++ ratemyprof.py
@@ -0,0 +1,60 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+def get_reviews(first_name, last_name):
+    search_url = "https://www.ratemyprofessors.com/search/professors/696?q=%s+%s" % (first_name, last_name)
+
+    resp = requests.get(search_url)
+
+    if resp.status_code == 200:
+        soup = BeautifulSoup(resp.content, "html.parser")
+        scripts = soup.find_all("script")
+        
+        for s in scripts:
+            stuff = str(s)
+            if "window.__RELAY_STORE__" in stuff:
+                i = stuff.find("legacyId")
+                end = stuff.index(",", i)
+                prof_id = stuff[i + 10:end]
+                break
+    else:
+        return None
+
+
+    prof_url = "https://www.ratemyprofessors.com/professor/" + prof_id
+
+    resp = requests.get(prof_url)
+    print(prof_url)
+    if resp.status_code == 200:
+        soup = BeautifulSoup(resp.content, "html.parser")
+        comments = soup.find_all(class_=lambda x: x and 'Comments' in x)
+        stripped_comments = [comment.text.strip() for comment in comments]
+        out_comments = ",".join(stripped_comments)
+        return out_comments
+    else:
+        return None
+
+
+
+def main():
+    with open("names.txt", "r") as f:
+        names = f.read().split("\n")
+
+    outputs = []
+    for name in names:
+        name_arr = name.split(", ")
+        first_name = name_arr[1]
+        last_name = name_arr[0]
+        reviews = get_reviews(first_name, last_name)
+        output = [first_name + " " + last_name, reviews]
+        outputs.append(output)
+
+    with open("reviews.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        for prof in outputs:
+            writer.writerow(prof)
+
+
+if __name__ == "__main__":
+    main()
